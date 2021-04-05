65cbcad5a302b480fbc096f537f83811d05ddf3e
Shreya388 <xjshreya39@gmail.com>
2021-04-05 18:13:57 +0530

    for loop

--- Sample Project/Script.js
+++ Sample Project/Script.js
@@ -1,10 +1,5 @@
+for(var i=1; i<40; i++){
+    console.log(i);
+}
 
- var text = "";
- var i = 0;
- while (i < 10) {
-   text += "<br>While you're at the store, get " + i + " packs of chips";
-   i++;
- }
- document.getElementById("demo").innerHTML = "<button class='btn btn-success'>"+text+"</button>";
-
- alert("A Wife says to her husband :) "); 
+alert(i);
