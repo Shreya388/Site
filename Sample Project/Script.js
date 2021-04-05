301e9019d79d42a1aff00b273458da1a3c905435
Shreya388 <xjshreya39@gmail.com>
2021-04-05 17:52:37 +0530

    made a while loop

---
+++ Sample Project/Script.js
@@ -0,0 +1,10 @@
+
+ var text = "";
+ var i = 0;
+ while (i < 10) {
+   text += "<br>While you're at the store, get " + i + " packs of chips";
+   i++;
+ }
+ document.getElementById("demo").innerHTML = "<button class='btn btn-success'>"+text+"</button>";
+
+ alert("A Wife says to her husband :) ");
