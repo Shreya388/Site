b2391951822f277475370d680576daec4ccde39b
Shreya388 <xjshreya39@gmail.com>
2021-04-29 20:06:30 +0530

    did something

---
+++ script.js
@@ -0,0 +1,75 @@
+//time values
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// vars to hold "display" value
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
+
+//var to hold setInterval()
+let interval = null;
+
+//var status for startStop function
+let status = "stopped";
+
+function stopWatch(){
+    seconds++;
+
+    //logic to determine when to increment next value
+    
+    if(seconds / 60 === 1){
+        seconds = 0;
+        minutes++;
+
+        if(minutes / 60 === 1){
+            minutes = 0;
+            hours++;
+        }
+    }
+
+
+    if(seconds < 10){
+        displaySeconds = "0" + seconds.toString();
+    }
+    else {
+        displaySeconds = seconds;
+    }
+
+    if(minutes < 10){
+        displayMinutes = "0" + minutes.toString();
+    }
+    else{
+        displayMinutes = minutes;
+
+    }
+
+    if(hours < 10){
+        displayHours = "0" + hours.toString();
+    }
+    else{
+        displayHours = hours;
+    }
+
+
+
+    //code to display the time values
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+}
+
+function startStop(){
+
+    if(status === "stopped"){
+        //start the stopwatch by the setInterval function
+        interval = window.setInterval(stopWatch, 1000);
+        document.getElementById("startStop").innerHTML = "Stop";
+        status = "started";
+    }
+    else{
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML = "Start";
+        status = "stopped";
+    }
+}
+    
