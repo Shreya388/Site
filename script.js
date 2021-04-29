6fe9ec2bdd5fb2c03911927b9f0792499b3c6da1
Shreya388 <xjshreya39@gmail.com>
2021-04-29 20:34:25 +0530

    made the reset button work

--- script.js
+++ script.js
@@ -73,3 +73,12 @@ function startStop(){
     }
 }
     
+// function for reset button
+function reset(){
+    window.clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.getElementById("display").innerHTML = "00:00:00";
+    document.getElementById("startStop").innerHTML = "Start";
+}
