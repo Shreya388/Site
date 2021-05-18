15d881e80f85105afd23aa23a6324a67f2f25aca
Shreya388 <xjshreya39@gmail.com>
2021-05-18 21:58:09 +0530

    now you can use : /contact in localhost:4040

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -4,9 +4,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("hello world!");
+    res.send("<button>Click</button>");
+});
+
+app.get("/contact", function(req, res){
+    res.send("<p>You can contact on: meenaxjha@gmail.com");
 });
 
 app.listen(4040, function(){
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 4040");
 });
