9d19816b5952868bdd7cd266250d448af9bbb336
Shreya388 <xjshreya39@gmail.com>
2021-05-18 22:01:34 +0530

    Added /about in localhost:4040

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -11,6 +11,10 @@ app.get("/contact", function(req, res){
     res.send("<p>You can contact on: meenaxjha@gmail.com");
 });
 
+app.get("/about", function(req, res){
+    res.send("<p>This is an express.js server");
+});
+
 app.listen(4040, function(){
     console.log("Server is running on port 4040");
 });
