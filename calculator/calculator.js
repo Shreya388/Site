c59b9e1d4836fa8e2bd797099d26570c01709553
Shreya388 <xjshreya39@gmail.com>
2021-05-19 23:09:48 +0530

    Made a web app

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,23 @@
 //jshint exversion:6
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+
+   //making a calc using var
+   var num1 = Number(req.body.num1);
+   var num2 = Number(req.body.num2);
+
+   //adding both variables in var named result
+   var result = num1 + num2; 
+    res.send("The result of your calculation is " + result);
 });
 
 
