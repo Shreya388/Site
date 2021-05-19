9b1bbbef946630f11205e12923681072ec743187
Shreya388 <xjshreya39@gmail.com>
2021-05-19 22:19:29 +0530

    removed /about from localhost:4040

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -7,13 +7,11 @@ app.get("/", function(req, res){
     res.send("<button>Click</button>");
 });
 
+
 app.get("/contact", function(req, res){
-    res.send("<p>You can contact on: meenaxjha@gmail.com");
+    res.send("<p>You can contact on: meenaxjha@gmail.com</p>");
 });
 
-app.get("/about", function(req, res){
-    res.send("<p>This is an express.js server");
-});
 
 app.listen(4040, function(){
     console.log("Server is running on port 4040");
