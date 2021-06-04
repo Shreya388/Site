0ac54f1b37d6f71bd6483bd5ee17466553e4475e
Shreya <xjshreya37@gmail.com>
2021-06-04 22:59:18 +0530

    made for loop

--- Sample Project/Script.js
+++ Sample Project/Script.js
@@ -1,1 +1,14 @@
 
+
+var fruits = ["apple", "mango", "orange", "volvo", "farari"];
+var text = "";
+var i;
+
+
+for (i = 0; i < fruits.length; i++) {
+    text += fruits[i] + "<br>";
+  
+}
+
+document.getElementById("demo").innerHTML = text;
+console.log();
