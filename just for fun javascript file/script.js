8c98ed2bd88b6b2fff70eb950be9920ca6acce7a
Shreya388 <xjshreya39@gmail.com>
2021-06-13 22:57:09 +0530

    making a javascript while loop practice

--- just for fun javascript file/script.js
+++ just for fun javascript file/script.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $(".div1").toggle();
-  });
-});
+var num = prompt('');
+var randomNum = Math.random();
+let a = 5;
+
+var totalNum = num + randomNum;
+
+
+
+var text = "";
+var i = 0;
+while (i < 20) {
+  
+  text += "<br>The number is " + totalNum;
+  i+=1;
+  
+}
+document.getElementById("demo").innerHTML = text;
